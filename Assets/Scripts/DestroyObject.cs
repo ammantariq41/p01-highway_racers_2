f20a290f8a8ec9fc36830d09b9b5c2a807016d26
Muhammad Hamza <muhammadhamza2310@gmail.com>
2023-01-29 15:54:55 +0500

    Added collision sound and particle effects on collision

--- Assets/Scripts/DestroyObject.cs
+++ Assets/Scripts/DestroyObject.cs
@@ -8,11 +8,14 @@ public class DestroyObject : MonoBehaviour
 {
     public TextMeshProUGUI timerText;
     float col_hit = 0;
+    public AudioSource audioSource;
+    public Transform dust;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        dust.GetComponent<ParticleSystem>().enableEmission = false;
     }
 
     // Update is called once per frame
@@ -21,15 +24,26 @@ public class DestroyObject : MonoBehaviour
     {
         if (other.gameObject.tag == "tri")
         {
-            col_hit = col_hit + 1;
-            if (col_hit == 4)
-            {
-                Destroy(gameObject);
-                SceneManager.LoadScene(2);
-            }
+            Debug.Log("Hit Obstacle");
+            audioSource.Play();
+            dust.GetComponent<ParticleSystem>().enableEmission = true;
+            StartCoroutine (stopSparkle1());
+
+            // col_hit = col_hit + 1;
+            // if (col_hit == 4)
+            // {
+            //     Destroy(gameObject);
+            //     SceneManager.LoadScene(2);
+            // }
         }
         timerText.text = "Hits Left" + col_hit.ToString() + "/4";
+    }
 
+    IEnumerator stopSparkle1()
+    {
+        Debug.Log("Dust");
+        yield return new WaitForSeconds (.4f);
+        dust.GetComponent<ParticleSystem>().enableEmission = false;
     }
 
 }
