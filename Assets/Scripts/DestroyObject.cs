bedff14e0b2ba8b7af53337cd0f4358c6a4c0a7f
ammantariq41 <ammantariq41@gmail.com>
2023-03-01 14:17:50 +0500

    intro video, fixed screen flow, better UI

--- Assets/Scripts/DestroyObject.cs
+++ Assets/Scripts/DestroyObject.cs
@@ -33,7 +33,7 @@ public class DestroyObject : MonoBehaviour
              if (col_hit == 4)
              {
                  Destroy(gameObject);
-                 SceneManager.LoadScene(2);
+                 SceneManager.LoadScene(3);
             }
         }
         timerText.text = "Hits" + col_hit.ToString() + "/4";
